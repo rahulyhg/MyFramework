52eb448ae77bf68b125410c3394b4efc868e598b
Єгор Крушевський <egorkrushevskuy@gmail.com>
2018-11-04 12:31:12 +0200

    add extension RabitMQ

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -10,6 +10,7 @@ return array(
     'Twig\\' => array($vendorDir . '/twig/twig/src'),
     'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
     'Predis\\' => array($vendorDir . '/predis/predis/src'),
+    'PhpAmqpLib\\' => array($vendorDir . '/php-amqplib/php-amqplib/PhpAmqpLib'),
     'PHPMailer\\PHPMailer\\' => array($vendorDir . '/phpmailer/phpmailer/src'),
     'Components\\' => array($baseDir . '/components'),
 );
